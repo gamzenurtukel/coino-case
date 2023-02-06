b36188c4c1bcba64fb88508860bc6f1952ec8ecd
Gamzenur Tükel <92482243+gamzenurtukel@users.noreply.github.com>
2023-02-06 18:17:22 +0300

    dropdown menu arrar created

---
+++ src/utils/DropdownMenuList.tsx
@@ -0,0 +1,17 @@
+export const dropdownMenuList = [
+  {
+    id: 1,
+    name: "Artan fiyat",
+    isCheck: false,
+  },
+  {
+    id: 2,
+    name: "Azalan fiyat",
+    isCheck: false,
+  },
+  {
+    id: 3,
+    name: "Yüksek puanlılar",
+    isCheck: false,
+  },
+];
