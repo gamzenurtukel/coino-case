42bfad3b4b5f99d7f78e416a3a1cd756606d9624
Gamzenur Tükel <92482243+gamzenurtukel@users.noreply.github.com>
2023-02-07 22:33:37 +0300

    carts store created

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "./slices/cartSlice";
 import productSlice from "./slices/productSlice";
 
 export const store = configureStore({
   reducer: {
     products: productSlice,
+    carts: cartSlice,
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
