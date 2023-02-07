490b9e242b69c30e38695037629e7920b4a94280
Gamzenur Tükel <92482243+gamzenurtukel@users.noreply.github.com>
2023-02-07 22:31:28 +0300

    add to cart function and operation created

--- src/components/productList/ProductList.tsx
+++ src/components/productList/ProductList.tsx
@@ -5,21 +5,29 @@ import Rating from "@mui/material/Rating";
 import { getAllProducts } from "../../redux/slices/productSlice";
 import { useSelector } from "react-redux";
 import { useState } from "react";
+import { useAppDispatch } from "../../redux/hook";
+import { addToCart } from "../../redux/slices/cartSlice";
 
 type IProps = any[] | any;
 
 const ProductList: React.FC = () => {
   const [isFavorite, setIsFavorite] = useState<Boolean>(false);
   const productList: IProps = useSelector(getAllProducts);
+  const dispatch = useAppDispatch();
 
   const favoriteButtonOnClick = () => {
     setIsFavorite(!isFavorite);
   };
+
+  const addToCartButton = (product: any) => {
+    dispatch(addToCart(product));
+  };
+
   return (
     <div className={style.productListWrapper}>
       <div className={style.product}>
         {productList?.map((product: any) => (
-          <div className={style.productContainer}>
+          <div className={style.productContainer} key={product.id}>
             <div className={style.productImage}>
               <img src={product.image} alt="product" className={style.img} />
               <button
@@ -54,7 +62,12 @@ const ProductList: React.FC = () => {
               </div>
             </div>
             <div className={style.productAddToCart}>
-              <button className={style.addToCartButton}>Sepete ekle</button>
+              <button
+                className={style.addToCartButton}
+                onClick={() => addToCartButton(product)}
+              >
+                Sepete ekle
+              </button>
             </div>
           </div>
         ))}
