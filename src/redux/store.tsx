e9934ea15b669c8e3d58a26a95382bdfc1b128b0
Gamzenur Tükel <92482243+gamzenurtukel@users.noreply.github.com>
2023-02-08 18:16:59 +0300

    favorites store created

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./slices/cartSlice";
+import favoriteSlice from "./slices/favoriteSlice";
 import productSlice from "./slices/productSlice";
 
 export const store = configureStore({
   reducer: {
     products: productSlice,
     carts: cartSlice,
+    favorites: favoriteSlice,
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
