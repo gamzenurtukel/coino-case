c29f40a987df8ef40443074427505ec157db6e3c
Gamzenur Tükel <92482243+gamzenurtukel@users.noreply.github.com>
2023-02-09 14:28:11 +0300

    shortSlice added

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./slices/cartSlice";
 import favoriteSlice from "./slices/favoriteSlice";
 import productSlice from "./slices/productSlice";
+import sortSlice from "./slices/sortSlice";
 
 export const store = configureStore({
   reducer: {
     products: productSlice,
     carts: cartSlice,
     favorites: favoriteSlice,
+    sorts: sortSlice,
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
